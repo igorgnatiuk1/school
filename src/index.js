55b60d01a5434f8dd34ad1200f82b3256991cf2c
Igor Gnatiuk <igor.gnatiuk111@gmail.com>
2022-09-29 07:47:35 +0300

    new

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import './public/index.css'
+
+import 'core-js/stable'
+import 'bootstrap'
+
+const div = document.querySelector('div')
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const  path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: '.src/index.js',
+    devtool: 'source-map',
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        publicPath: "/",
+        filename: "bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [['@babel/preset-env', {targets: 'defaults'}]],
+                    },
+                },
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/public/index.html'),
+            filename: "index.html",
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, './src'),
+        },
+        port: 63342,
+        open: false,
+    },
+}
